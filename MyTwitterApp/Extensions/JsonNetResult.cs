d16943961937dbab67355597e49df32614201f5b
Arindam, Ghosh <arindam.ghosh@cerner.com>
2017-10-24 23:31:19 -0500

    custom action result

---
+++ MyTwitterApp/Extensions/JsonNetResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace MyTwitterApp.Extensions
+{
+    public class JsonNetResult : ActionResult
+    {
+        public object Data { get; set; }
+        public override void ExecuteResult(ControllerContext context)
+        {
+            var response = context.HttpContext.Response;
+            response.ContentType = "application/json";
+            response.Write(JsonConvert.SerializeObject(Data,new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() }));
+
+        }
+    }
+}
